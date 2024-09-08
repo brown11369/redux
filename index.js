6c70bf17ca0a4bde25f0222d93c39faa72ee67a5
Brown <63307642+brown11369@users.noreply.github.com>
2024-09-08 23:48:17 +0530

    add action type and their functionality

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import {createStore} from "redux";
+import { createStore } from "redux";
 
-const reducer=(state={amount:0},action)=>{
+// action type
+const increment = "increment"
+const incrementByValue = "incrementByValue"
+const decrement = "decrement"
+const decrementByValue = "decrementByValue"
+
+const reducer = (state = { amount: 0 }, action) => {
+    switch (action.type) {
+        case increment:
+            return { amount: state.amount + 1 }
+        case incrementByValue:
+            return { amount: state.amount + action.payload }
+        case decrement:
+            return { amount: state.amount - 1 }
+        case decrementByValue:
+            return { amount: state.amount - action.payload }
+        default:
+            return state
+    }
     return state
 }
-const store=createStore(reducer)
+const store = createStore(reducer)
 
 console.log(store.getState())
-
